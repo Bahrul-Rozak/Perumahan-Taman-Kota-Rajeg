828ed432320744955a8b8d51f659d685beb30e8b
Bahrul Rozak <bahrulrozak13062001@gmail.com>
2023-03-22 11:13:42 +0700

    model & migration slider

---
+++ admin-perum-taman-kota-rajeg/app/Models/Slider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Slider extends Model
+{
+    use HasFactory;
+
+    /**
+     * guarded
+     *
+     * @var array
+     */
+    protected $guarded = [];
+
+    /**
+     * image
+     *
+     * @return Attribute
+     */
+    protected function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => asset('/storage/sliders/' . $value),
+        );
+    }
+}
